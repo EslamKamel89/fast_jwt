fad95b123ee7856036d23940b8ce7ca05939a9d6
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-15 11:00:49 +0200

    feat(db): add async database session setup

---
+++ app/db/session.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
+
+from app.core.config import settings
+
+engine = create_async_engine(
+    url=settings.ASYNC_DATABASE_URL , 
+    pool_pre_ping=True,
+    echo=True , 
+    future=True)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine , 
+    class_=AsyncSession , 
+    expire_on_commit=False , 
+    autoflush=False , 
+    autocommit=False)
+
+async def get_session():
+    async with AsyncSessionLocal() as session:
+        yield session
