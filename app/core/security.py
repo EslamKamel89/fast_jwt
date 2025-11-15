a0701f46b0de07df141181d092676c05ad6adc21
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-15 11:41:17 +0200

    feat(security): add password hashing and verification utilities

---
+++ app/core/security.py
@@ -0,0 +1,7 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'] , depreciated='auto')
+def has_password(password:str)->str :
+    return pwd_context.hash(password)
+def verify_password(plain:str , hashed:str)->bool :
+    return pwd_context.verify(plain , hashed)
