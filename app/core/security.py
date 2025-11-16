151ce1b720c667dfd140a9e7dbb92e8619c0b692
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-16 06:33:44 +0200

    feat(security): add JWT access token generation

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,22 @@
+from datetime import datetime, timedelta
+from typing import Any, Dict
+
+from jose import jwt
 from passlib.context import CryptContext
+
+from app.core.config import settings
 
 pwd_context = CryptContext(schemes=['bcrypt'] , depreciated='auto')
 def has_password(password:str)->str :
     return pwd_context.hash(password)
 def verify_password(plain:str , hashed:str)->bool :
     return pwd_context.verify(plain , hashed)
+
+def create_access_token(subject:str, extra: Dict[str , Any]|None = None) -> str : 
+    payload:Dict[str, Any] = {'sub':subject}
+    if extra : 
+        payload.update(extra)
+    expire = datetime.now() + timedelta(minutes=settings.ACCESS_EXPIRE_MINUTES)
+    payload['expire'] = expire
+    return jwt.encode(claims=payload , key=settings.JWT_SECRET , algorithm=settings.JWT_ALG)
+        
