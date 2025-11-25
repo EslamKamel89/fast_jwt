74fbcc57a4d923a19f739bf738b6818cd6393fe8
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-25 13:53:59 +0200

    feat(sandbox): implement lesson1 categories endpoint with DB query

--- app/apps/sandbox/router.py
+++ app/apps/sandbox/router.py
@@ -1,12 +1,17 @@
+from typing import Sequence
+
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.apps.sandbox.models import Category
 from app.db.session import get_session
 
 router = APIRouter(prefix='/sandbox' , tags=['sandbox'])
 
-@router.get('')
-async def sandbox(session:AsyncSession=Depends(get_session)):
-    return {"data":{
-        "message":"it works"
-    }}
+@router.get('/lesson1/categories')
+async def sandbox(session:AsyncSession=Depends(get_session)): 
+    stmt = select(Category).order_by(Category.name)
+    res = await session.execute(stmt)
+    categories:Sequence[Category] = res.scalars().all()
+    return {"data":{"categories":categories }} 
